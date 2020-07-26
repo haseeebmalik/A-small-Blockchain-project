0defeb72090b26ffb405558adebd73b468b5d110
haseeebmalik <m.haseebmalik123@gmail.com>
2020-07-26 08:04:40 +0500

    Update mainProject.js
    some bug fixes

--- mainProject.js
+++ mainProject.js
@@ -1,6 +1,6 @@
 
 /*in this code we are accessing the functions of one of our deployed
-contract on ropstin .this is not transaction ,we are only reading Data.
+contract on ropstin .This is not transaction ,we are only reading Data.
 In this case we are using abi.Here web3 is connecting us 
 with infura's ropstin node.
 we are accessing blockchain data on web browser.
@@ -8,6 +8,8 @@ we are accessing blockchain data on web browser.
 
 
 function myFun(){
+
+var write = document.getElementById('h2');
 	
 
 	
@@ -22,7 +24,7 @@ function myFun(){
 
 
 
-
+//infura's node end point 
 const rpcURL="https://ropsten.infura.io/v3/6f37c18a549041e6a64e2b5b1b938b8d";
 
 let web3=new Web3(rpcURL);
@@ -74,7 +76,7 @@ const contract = new web3.eth.Contract(abi, address);
 
 // calling the dosomework function of contract 
 
-var write = document.getElementById('h2');
+
 contract.methods.doSomeThing().call(function(err,result){
 		
 	 if(err){
